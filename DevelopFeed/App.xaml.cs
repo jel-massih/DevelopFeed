9a08704eeadfe0e2683f4099e82c06b79378a1ea
Jason El-Massih <jel-massih@hotmail.com>
2013-11-20 19:10:27 -0500

    Initial Commit

---
+++ DevelopFeed/App.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Telerik.Windows.Controls;
+
+namespace DevelopFeed
+{
+    public partial class App : Application
+    {
+        public RadDiagnostics diagnostics;
+
+        public RadRateApplicationReminder rateReminder;
+
+        public PhoneApplicationFrame RootFrame { get; private set; }
+
+        public App()
+        {
+            UnhandledException += Application_UnhandledException;
+
+            InitializeComponent();
+
+            InitializePhoneApplication();
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Application.Current.Host.Settings.EnableFrameRateCounter = true;
+
+                PhoneApplicationService.Current.UserIdleDetectionMode = IdleDetectionMode.Disabled;
+            }
+
+            diagnostics = new RadDiagnostics();
+            diagnostics.EmailTo = "jel-massih@Hotmail.com";
+            diagnostics.Init();
+
+            rateReminder = new RadRateApplicationReminder();
+            rateReminder.RecurrencePerUsageCount = 5;
+            rateReminder.AllowUsersToSkipFurtherReminders = false;
+        }
+
+        private void Application_Launching(object sender, LaunchingEventArgs e)
+        {
+            ApplicationUsageHelper.Init("1.0");
+        }
+
+        private void Application_Activated(object sender, ActivatedEventArgs e)
+        {
+            if (!e.IsApplicationInstancePreserved)
+            {
+                ApplicationUsageHelper.OnApplicationActivated();
+            }
+        }
+
+        // Code to execute when the application is deactivated (sent to background)
+        // This code will not execute when the application is closing
+        private void Application_Deactivated(object sender, DeactivatedEventArgs e)
+        {
+        }
+
+        // Code to execute when the application is closing (eg, user hit Back)
+        // This code will not execute when the application is deactivated
+        private void Application_Closing(object sender, ClosingEventArgs e)
+        {
+            // Ensure that required application state is persisted here.
+        }
+
+        // Code to execute if a navigation fails
+        private void RootFrame_NavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                // A navigation has failed; break into the debugger
+                System.Diagnostics.Debugger.Break();
+            }
+        }
+
+        // Code to execute on Unhandled Exceptions
+        private void Application_UnhandledException(object sender, ApplicationUnhandledExceptionEventArgs e)
+        {
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                // An unhandled exception has occurred; break into the debugger
+                System.Diagnostics.Debugger.Break();
+            }
+        }
+
+        #region Phone application initialization
+
+        // Avoid double-initialization
+        private bool phoneApplicationInitialized = false;
+
+        // Do not add any additional code to this method
+        private void InitializePhoneApplication()
+        {
+            if (phoneApplicationInitialized)
+                return;
+
+            // Create the frame but don't set it as RootVisual yet; this allows the splash
+            // screen to remain active until the application is ready to render.
+            RootFrame = new PhoneApplicationFrame();
+            RootFrame.Navigated += CompleteInitializePhoneApplication;
+
+            // Handle navigation failures
+            RootFrame.NavigationFailed += RootFrame_NavigationFailed;
+
+            // Ensure we don't initialize again
+            phoneApplicationInitialized = true;
+        }
+
+        // Do not add any additional code to this method
+        private void CompleteInitializePhoneApplication(object sender, NavigationEventArgs e)
+        {
+            // Set the root visual to allow the application to render
+            if (RootVisual != RootFrame)
+                RootVisual = RootFrame;
+
+            // Remove this handler since it is no longer needed
+            RootFrame.Navigated -= CompleteInitializePhoneApplication;
+        }
+
+        #endregion
+    }
+}
---
+++ DevelopFeed/MainPage.xaml.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Tasks;
+using System.Xml;
+using System.ServiceModel.Syndication;
+using System.IO;
+using System.Collections.ObjectModel;
+
+namespace DevelopFeed
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        private ObservableCollection<SyndicationItem> newsItems;
+        private ObservableCollection<SyndicationItem> featuresItems;
+        private ObservableCollection<SyndicationItem> blogItems;
+
+        private const String FAILED_CONNECTION_STRING = "Unable To Connect To Network.";
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            if (NavigationContext == null || NavigationContext.QueryString == null || NavigationContext.QueryString.Count == 0)
+            {
+                (App.Current as App).rateReminder.Notify();
+            }
+
+            if (!System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable())
+            {
+                MessageBox.Show("No Network Connection Available! This App Requires an active network Connection!");
+            }
+
+            this.Loaded += new RoutedEventHandler(MainPage_Loaded);
+        }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        void client_newsDownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                newsFeedListBox.EmptyContent = FAILED_CONNECTION_STRING;
+            }
+            else
+            {
+                this.State["newsFeed"] = e.Result;
+                UpdateNewsFeedList(e.Result);
+            }
+        }
+
+        void client_featuresDownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                featuresFeedListBox.EmptyContent = FAILED_CONNECTION_STRING;
+            }
+            else
+            {
+                this.State["featuresFeed"] = e.Result;
+                UpdateFeaturesFeedList(e.Result);
+            }
+        }
+
+        void client_blogDownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                blogFeedListBox.EmptyContent = FAILED_CONNECTION_STRING;
+            }
+            else
+            {
+                this.State["blogFeed"] = e.Result;
+                UpdateBlogFeedList(e.Result);
+            }
+        }
+
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            if (this.State.ContainsKey("newsFeed"))
+            {
+                if (newsFeedListBox.ItemCount == 0)
+                {
+                    UpdateNewsFeedList(State["newsFeed"] as string);
+                }
+            }
+            if (this.State.ContainsKey("featuresFeed"))
+            {
+                if (newsFeedListBox.ItemCount == 0)
+                {
+                    UpdateFeaturesFeedList(State["featuresFeed"] as string);
+                }
+            }
+            if (this.State.ContainsKey("blogFeed"))
+            {
+                if (newsFeedListBox.ItemCount == 0)
+                {
+                    UpdateBlogFeedList(State["blogFeed"] as string);
+                }
+            }
+        }
+
+        private void UpdateNewsFeedList(string feedXML)
+        {
+            StringReader stringReader = new StringReader(feedXML);
+            XmlReader xmlReader = XmlReader.Create(stringReader);
+
+            newsItems.Clear();
+            
+            try
+            {
+                SyndicationFeed feed = SyndicationFeed.Load(xmlReader);
+
+                foreach (SyndicationItem item in feed.Items)
+                {
+                    newsItems.Insert(0, item);
+                }
+            }
+            catch (Exception) { newsFeedListBox.EmptyContent = FAILED_CONNECTION_STRING; }
+
+            Deployment.Current.Dispatcher.BeginInvoke(() =>
+            {
+                newsFeedListBox.StopPullToRefreshLoading(true);
+            });
+        }
+
+        private void UpdateFeaturesFeedList(string feedXML)
+        {
+            StringReader stringReader = new StringReader(feedXML);
+            XmlReader xmlReader = XmlReader.Create(stringReader);
+            
+            featuresItems.Clear();
+            try
+            {
+                SyndicationFeed feed = SyndicationFeed.Load(xmlReader);
+
+                foreach (SyndicationItem item in feed.Items)
+                {
+                    featuresItems.Insert(0, item);
+                }
+            }
+            catch (Exception) { featuresFeedListBox.EmptyContent = FAILED_CONNECTION_STRING; }
+
+            Deployment.Current.Dispatcher.BeginInvoke(() =>
+            {
+                featuresFeedListBox.StopPullToRefreshLoading(true);
+            });
+        }
+
+        private void UpdateBlogFeedList(string feedXML)
+        {
+            StringReader stringReader = new StringReader(feedXML);
+            XmlReader xmlReader = XmlReader.Create(stringReader);
+            
+            blogItems.Clear();
+            try
+            {
+                SyndicationFeed feed = SyndicationFeed.Load(xmlReader);
+
+                foreach (SyndicationItem item in feed.Items)
+                {
+                    blogItems.Insert(0, item);
+                }
+            }
+            catch (Exception) { blogFeedListBox.EmptyContent = FAILED_CONNECTION_STRING; }
+
+            Deployment.Current.Dispatcher.BeginInvoke(() =>
+            {
+                blogFeedListBox.StopPullToRefreshLoading(true);
+            });
+        }
+
+        private void FeedListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Telerik.Windows.Controls.RadDataBoundListBox box = sender as Telerik.Windows.Controls.RadDataBoundListBox;
+            if (box != null && box.SelectedItem != null)
+            {
+                SyndicationItem sItem = (SyndicationItem)box.SelectedItem;
+
+                if (sItem.Links.Count > 0)
+                {
+                    Uri uri = sItem.Links.FirstOrDefault().Uri;
+
+                    WebBrowserTask task = new WebBrowserTask();
+                    task.Uri = uri;
+                    task.Show();
+                }
+            }
+        }
+
+        private void newsPivotItem_Loaded(object sender, RoutedEventArgs e)
+        {
+            newsItems = new ObservableCollection<SyndicationItem>();
+            newsFeedListBox.ItemsSource = newsItems;
+            WebClient client = new WebClient();
+            client.DownloadStringCompleted += client_newsDownloadStringCompleted;
+            client.DownloadStringAsync(new System.Uri("http://feeds.feedburner.com/developmag/ifbh"));
+        }
+
+        private void featuresPivotItem_Loaded(object sender, RoutedEventArgs e)
+        {
+            featuresItems = new ObservableCollection<SyndicationItem>();
+            featuresFeedListBox.ItemsSource = featuresItems;
+            WebClient client = new WebClient();
+            client.DownloadStringCompleted += client_featuresDownloadStringCompleted;
+            client.DownloadStringAsync(new System.Uri("http://feeds.feedburner.com/develop/features"));
+        }
+
+        private void blogPivotItem_Loaded(object sender, RoutedEventArgs e)
+        {
+            blogItems = new ObservableCollection<SyndicationItem>();
+            blogFeedListBox.ItemsSource = blogItems;
+            WebClient client = new WebClient();
+            client.DownloadStringCompleted += client_blogDownloadStringCompleted;
+            client.DownloadStringAsync(new System.Uri("http://feeds.feedburner.com/develop-online/game-development-news"));
+        }
+
+        private void newsFeedListBox_RefreshRequested(object sender, EventArgs e)
+        {
+            WebClient client = new WebClient();
+            client.DownloadStringCompleted += client_newsDownloadStringCompleted;
+            client.DownloadStringAsync(new System.Uri("http://feeds.feedburner.com/developmag/ifbh"));
+        }
+
+        private void featuresFeedListBox_RefreshRequested(object sender, EventArgs e)
+        {
+            WebClient client = new WebClient();
+            client.DownloadStringCompleted += client_featuresDownloadStringCompleted;
+            client.DownloadStringAsync(new System.Uri("http://feeds.feedburner.com/develop/features"));
+        }
+
+        private void blogFeedListBox_RefreshRequested(object sender, EventArgs e)
+        {
+            WebClient client = new WebClient();
+            client.DownloadStringCompleted += client_blogDownloadStringCompleted;
+            client.DownloadStringAsync(new System.Uri("http://feeds.feedburner.com/develop-online/game-development-news"));
+        }
+    }
+}
